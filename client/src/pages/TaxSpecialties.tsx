6cf234ba49b57608818af0b16d1871329e324299
Bill Wright <jordanad46@gmail.com>
2026-03-10 21:41:39 -0400

    Update TaxSpecialties.tsx

--- client/src/pages/TaxSpecialties.tsx
+++ client/src/pages/TaxSpecialties.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams() } from "wouter";
+import { useParams } from "wouter";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SubpageNav from "@/components/SubpageNav";
